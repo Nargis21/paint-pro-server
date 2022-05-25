1c8089fab6d99723eda2f06ea09887e47e9ce925
Nargis21 <nargisakther1933@gmail.com>
2022-05-25 17:44:44 +0600

    dashboard api added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -12,16 +13,83 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmgr5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    });
+
+}
+
 async function run() {
     try {
         await client.connect()
         console.log('DB Connected!')
-        const toolsCollection = client.db('paint-pro').collection('tools')
+        const toolCollection = client.db('paint-pro').collection('tools')
+        const orderCollection = client.db('paint-pro').collection('orders')
+        const userCollection = client.db('paint-pro').collection('users')
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+
+            res.send({ result, token })
+
+        })
 
         app.get('/tool', async (req, res) => {
-            const tools = await toolsCollection.find().toArray()
+            const tools = await toolCollection.find().toArray()
             res.send(tools)
         })
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const tool = await toolCollection.findOne(query)
+            res.send(tool)
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        })
+
+        app.get('/order', verifyJWT, async (req, res) => {
+            const userEmail = req.query.email
+            const decodedEmail = req.decoded.email
+            if (decodedEmail === userEmail) {
+                const query = { email: userEmail }
+                const orders = await orderCollection.find(query).toArray()
+                res.send(orders)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+        })
+
+        app.delete('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
     }
     finally {
